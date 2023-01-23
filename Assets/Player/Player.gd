540da39827cfb9f966a3f2ca08e009c99b016891
bananapix <jwoods4613@gmail.com>
2023-01-23 08:46:06 -0700

    Stabilized
    Made some fixes to other issues derived from migrating godot versions

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 #var velocity = Vector2.ZERO
 var mouse_is_close = true
+var mouse_lerp_size = Vector2.ZERO
 
 @onready var animations = $AnimationPlayer
 @onready var states = $State_Manager
@@ -15,6 +16,7 @@ func _ready() -> void:
 	# that way they can move and react accordingly
 	states.init(self)
 	mouse_is_close = false
+	mouse_lerp_size = get_viewport().size
 
 func _unhandled_input(event: InputEvent) -> void:
 	states.input(event)
@@ -22,7 +24,7 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	states.physics_process(delta)
 	if (mouse_is_close == false):
-		var mouse_offset = (get_viewport().get_mouse_position() - Vector2(get_viewport().size / 8))
+		var mouse_offset = (get_viewport().get_mouse_position() - Vector2(mouse_lerp_size / 8))
 		$Camera2D.position = lerp(Vector2(), mouse_offset.normalized() * offset_normalize, mouse_offset.length() / camera_lerp_magnitude)
 
 func _process(delta: float) -> void:
@@ -31,7 +33,7 @@ func _process(delta: float) -> void:
 
 func _mouse_entered():
 	mouse_is_close = true
-	var mouse_offset = (get_viewport().get_mouse_position() - Vector2(get_viewport().size / 8))
+	var mouse_offset = (get_viewport().get_mouse_position() - Vector2(mouse_lerp_size / 8))
 	$Camera2D.position = lerp(Vector2(), Vector2.ZERO, mouse_offset.length() / camera_lerp_magnitude)
 
 func _mouse_exited():
