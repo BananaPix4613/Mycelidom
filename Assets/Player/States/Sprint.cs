c834ce147ca18d9fde649bb85fd19fd0947bb21a
Eric Yohansa <eric.yohansa@outlook.com>
2023-06-16 19:14:35 +0700

    Rewrite sprint

--- Assets/Player/States/Sprint.cs
+++ Assets/Player/States/Sprint.cs
@@ -2,5 +2,29 @@ using Godot;
 
 public partial class Sprint : Move
 {
+    public override void Enter()
+    {
+        base.Enter();
+        Player.CurrentState(Sprint);
+        Player.Animations.SpeedScale = 1.5f;
+    }
 
+    public override void Exit()
+    {
+        base.Exit();
+        Player.Animations.SpeedScale = 1;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (NewState == null)
+        {
+            if (Input.IsActionJustReleased("sprint"))
+            {
+                NewState = Walk;
+            }
+            NewState = null;
+        }
+    }
 }
