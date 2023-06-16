ee56ebcf899b7508b41e69a2cd6f10fe98204ec0
Eric Yohansa <eric.yohansa@outlook.com>
2023-06-16 19:23:18 +0700

    Rewrite walk

--- Assets/Player/States/Walk.cs
+++ Assets/Player/States/Walk.cs
@@ -2,5 +2,22 @@ using Godot;
 
 public partial class Walk : Move
 {
+    public override void Enter()
+    {
+        base.Enter();
+        Player.CurrentState(Walk);
+        Player.Animations.SpeedScale = 1;
+    }
 
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (NewState == null)
+        {
+            if (Input.IsActionPressed("sprint"))
+                NewState = Sprint;
+            else
+                NewState = null;
+        }
+    }
 }
